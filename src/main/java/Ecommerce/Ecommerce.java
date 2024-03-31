57a8cf47f71a8ceb5d7e7e5bce43faef1dec26d1
medhaholla <holla.medha@gmail.com>
2024-03-31 11:46:40 +0530

    feat: Added BaseModel with auditing attributes

---
+++ src/main/java/Ecommerce/Ecommerce.java
@@ -0,0 +1,13 @@
+package Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce.class, args);
+	}
+
+}
